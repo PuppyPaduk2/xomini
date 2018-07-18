b2d1e38d764ce5c7e608eaa6543c36b758441ec9
puppypaduk2 <debraooclark@gmail.com>
2018-07-19 00:21:15 +0300

    ref Notify

--- src/state.io/static/test/notify.js
+++ src/state.io/static/test/notify.js
@@ -6,7 +6,7 @@ let notify1, notify2;
 describe('Notify', () => {
    it('new', () => {
       notify1 = new Notify({
-         once: {
+         on: {
             event1: () => {
                console.log('@event1');
             },
@@ -21,46 +21,7 @@ describe('Notify', () => {
          }
       });
       notify2 = new Notify();
-   });
 
-   it('simple', () => {
-      notify1.once({
-         event1: () => {
-            console.log('@event1.2');
-         }
-      });
-      notify1.once({
-         event1: () => {
-            console.log('@event1:3:notify2');
-         }
-      }, notify2);
-
-      notify1.emit('event1');
-      notify1.emit('event2');
-      notify2.emit('event1');
-      notify1.emit('event1');
-      notify1.emit('event2');
-      notify2.emit('event1');
-   });
-
-   it('off()', () => {
-      notify1.on({
-         event1: () => {
-            console.log('@event1:3:notify2');
-         }
-      }, notify2);
-      notify1.on({
-         event1: () => {
-            console.log('@event1:3:notify2');
-         }
-      });
-
-      console.log('@1', notify1.countHandlers);
-      console.log('@2', notify2.countHandlers);
-
-      notify1.off(notify2);
-
-      console.log('@1', notify1.countHandlers);
-      console.log('@2', notify2.countHandlers);
+      console.log(notify1.handlers);
    });
 });
