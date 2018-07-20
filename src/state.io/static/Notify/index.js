0bf5719d5770d004b3c50a1ea30cff95cac64b8b
puppypaduk2 <debraooclark@gmail.com>
2018-07-20 17:43:03 +0300

    setup Notify

--- src/state.io/static/Notify/index.js
+++ src/state.io/static/Notify/index.js
@@ -8,14 +8,17 @@ export default class Notify {
     */
    constructor(options = {}) {
       let handlers = {};
-      let on = {};
-      let once = {};
       let listeners = {};
 
       defProps(this, {
+         listeners: {
+            get: () => {
+               return listeners;
+            }
+         },
          handlers: {
             get: () => {
-               return { on: on, once: once };
+               return handlers;
             }
          },
          event: {
@@ -25,7 +28,24 @@ export default class Notify {
              * @param {Array} event.args
              */
             set: (event) => {
-               console.log(event);
+               let isBreak = false;
+               let handlerResult;
+
+               handlersFilter(handlers, event.name, event.context, (handler) => {
+                  if (!isBreak) {
+                     if (handlerResult !== undefined) {
+                        handlerResult = handler.callback.call(this, handlerResult, ...event.args);
+                     } else {
+                        handlerResult = handler.callback.apply(this, event.args);
+                     }
+
+                     if (handlerResult instanceof Error) {
+                        isBreak = true;
+                     }
+
+                     return !handler.once;
+                  }
+               });
             }
          }
       });
@@ -35,27 +55,42 @@ export default class Notify {
        * @param {Notify} [context]
        */
       this.on = (addHandlers = {}, context = null) => {
-         Object.keys(addHandlers).forEach(nameEvent => {
-            if (context instanceof Notify) {
-               context.addListener(this, nameEvent);
-            } else {
-               this.addListener(this, nameEvent);
-            }
+         onHadlers.call(this, handlers, addHandlers, context);
+         return this;
+      };
 
-            let addHandlersEvent = addHandlers[nameEvent];
+      /**
+       * @param {Object} addHandlers
+       * @param {Notify} [context]
+       */
+      this.once = (addHandlers = {}, context = null) => {
+         onHadlers.call(this, handlers, addHandlers, context, true);
+         return this;
+      };
 
-            if (!(addHandlersEvent instanceof Array)) {
-               addHandlersEvent = [addHandlersEvent];
-            }
-
-            addHandlersEvent.forEach(handler => {
-               eventExist(handlers, nameEvent);
-               eventExist(on, nameEvent);
-
-               handlers[nameEvent].push(handler);
-               on[nameEvent].push(handler);
+      /**
+       * @param {Object} subHandlers
+       */
+      this.off = (subHandlers, context = null) => {
+         if (subHandlers === undefined) {
+            Object.keys(handlers).forEach(nameEvent => {
+               handlersFilter.call(this, handlers, nameEvent, null, () => false);
             });
-         });
+         } else if (subHandlers instanceof Notify) {
+            Object.keys(handlers).forEach(nameEvent => {
+               handlersFilter.call(this, handlers, nameEvent, subHandlers, () => false);
+            });
+         } else if (subHandlers instanceof Object) {
+            Object.keys(subHandlers).forEach(nameEvent => {
+               let subHandlersEvent = subHandlers[nameEvent] instanceof Array
+                  ? subHandlers[nameEvent]
+                  : [subHandlers[nameEvent]];
+
+               handlersFilter.call(this, handlers, nameEvent, context, (handler) => {
+                  return subHandlersEvent.indexOf(handler.callback) === -1;
+               });
+            });
+         }
 
          return this;
       };
@@ -72,6 +107,22 @@ export default class Notify {
                listeners[nameEvent].push(listener);
             }
          }
+         return this;
+      };
+
+      /**
+       * @param {Notify} listener
+       * @param {String} nameEvent 
+       */
+      this.subListener = (listener, nameEvent) => {         
+         if (typeof nameEvent === 'string' && listener instanceof Notify) {
+            const index = listeners[nameEvent].indexOf(listener);
+
+            if (index !== -1) {
+               listeners[nameEvent].splice(index, 1);
+            }
+         }
+         return this;
       };
 
       /**
@@ -84,8 +135,9 @@ export default class Notify {
             if (listenersEvent instanceof Array) {
                listenersEvent.forEach(listener => {
                   listener.event = {
-                     nameEvent: nameEvent,
-                     args: args
+                     name: nameEvent,
+                     args: args,
+                     context: this
                   };
                });
             }
@@ -95,6 +147,7 @@ export default class Notify {
       };
 
       this.on(options.on);
+      this.once(options.once);
    };
 };
 
@@ -106,4 +159,116 @@ function eventExist(handlers, nameEvent) {
    if (!(handlers[nameEvent] instanceof Array)) {
       handlers[nameEvent] = [];
    }
+};
+
+/**
+ * @param {Object} handlers
+ * @param {Object} addHandlers
+ * @param {Notify} [context]
+ * @param {Boolean} [once]
+ */
+function onHadlers(handlers, addHandlers = {}, context = null, once = false) {
+   Object.keys(addHandlers).forEach(nameEvent => {
+      if (!(context instanceof Notify)) {
+         context = this;
+      }
+
+      context.addListener(this, nameEvent);
+
+      let addHandlersEvent = addHandlers[nameEvent];
+
+      if (!(addHandlersEvent instanceof Array)) {
+         addHandlersEvent = [addHandlersEvent];
+      }
+
+      addHandlersEvent.forEach(handler => {
+         handler = {
+            callback: handler,
+            context: context,
+            once: !!once
+         };
+
+         eventExist(handlers, nameEvent);
+         handlers[nameEvent].push(handler);
+      });
+   });
+};
+
+/**
+ * 
+ * @param {Object} handlers
+ * @param {Object} handlersOnce
+ * @param {String} nameEvent
+ * @param {notify} context
+ * @param {Function} callback
+ */
+function handlersFilter(handlers, nameEvent, context = null, callback) {
+   const notremoveContext = {};
+   const removeContext = {};
+
+   if (handlers && handlers[nameEvent] instanceof Array) {
+      handlers[nameEvent] = handlers[nameEvent].filter((handler, index) => {
+         let result = true;
+
+         if (handler.context === context || context === null) {
+            if (callback instanceof Function) {
+               result = callback.call(this, handler);
+            }
+
+            result = typeof result === 'boolean' ? result : true;
+         }
+
+         eventExist(notremoveContext, nameEvent);
+         eventExist(removeContext, nameEvent);
+
+         if (result) {
+            let index = removeContext[nameEvent].indexOf(handler.context);
+
+            notremoveContext[nameEvent].push(handler.context);
+
+            if (index !== -1) {
+               removeContext[nameEvent].splice(index, 1);
+            }
+         } else if (notremoveContext[nameEvent].indexOf(handler.context) === -1) {
+            removeContext[nameEvent].push(handler.context);
+         }
+
+         if (handlers[nameEvent].length - 1 === index) {
+            if (!notremoveContext[nameEvent].length) {
+               delete notremoveContext[nameEvent];
+            }
+
+            if (!removeContext[nameEvent].length) {
+               delete removeContext[nameEvent];
+            }
+         }
+
+         return result;
+      });
+
+      if (!handlers[nameEvent].length) {
+         delete handlers[nameEvent];
+      }
+   }
+
+   Object.keys(removeContext).forEach(nameEvent => {
+      removeContext[nameEvent].forEach(context => {
+         context.subListener(this, nameEvent);
+      });
+   });
+
+   return (callback) => {
+      callback.call(this, removeContext, notremoveContext);
+   };
+};
+
+/**
+ * @param {Object} contexts
+ */
+function removeContexts(contexts) {
+   Object.keys(contexts).forEach(nameEvent => {
+      contexts[nameEvent].forEach(context => {
+         context.subListener(this, nameEvent);
+      });
+   });
 };
--- src/state.io/static/test/notify.js
+++ src/state.io/static/test/notify.js
@@ -6,28 +6,131 @@ let notify1, notify2;
 describe('Notify', () => {
    it('new', () => {
       notify1 = new Notify();
-      notify2 = new Notify({
-         on: {
-            event1: () => {
-               console.log('@event1');
-            },
-            event2: [
-               () => {
-                  console.log('@event2:1');
-               },
-               () => {
-                  console.log('@event2:2');
-               }
-            ]
-         }
-      });
+      notify2 = new Notify();
 
       notify1.on({
-         event1: () => {
-            console.log('@event1:1');
-         }
+         event1: [(value) => {
+            console.log('@event1:1', value);
+            return value + ' add value!';
+         }, (value) => {
+            console.log('@event1:2', value);
+         }]
       }, notify2);
 
       notify2.emit('event1', 1, '2', { a: 3 });
+      notify1.emit('event1', 1, '2', { a: 3 });
+
+      notify1.once({
+         event1: (...args) => {
+            console.log('event1:once', ...args);
+         }
+      });
+
+      notify1.emit('event1', 1, '2', { a: 3 });
+      notify1.emit('event1', 1, '2', { a: 3 });
+   });
+
+   it('emit with error', () => {
+      notify1 = new Notify({
+         on: {
+            event: [
+               () => {
+                  console.log('@event_1');
+               },
+               (value) => {
+                  console.log('@event_2');
+
+                  if (!value) {
+                     return new Error();
+                  }
+               }
+            ]
+         },
+         once: {
+            event: () => {
+               console.log('@event_after_error');
+            }
+         }
+      });
+
+      notify1.emit('event').emit('event', true)
+         .emit('event', true);
+   });
+
+   it('off', () => {
+      notify1 = new Notify({
+         on: {
+            event: () => { }
+         }
+      });
+      notify2 = new Notify();
+      let notify3 = new Notify();
+      let func1 = () => { }
+
+      notify2.on({
+         event: [func1, () => { }]
+      }, notify1);
+
+      notify2.once({
+         event: () => { }
+      }, notify3);
+
+      notify2.off({
+         event: func1
+      }, notify1);
+
+      console.log('@notify1', notify1.listeners.event.length);
+
+      notify2.off();
+
+      console.log('@notify1', notify1.listeners.event.length);
+
+      notify2.once({
+         event: () => { }
+      }, notify3);
+
+      console.log('@notify3', notify3.listeners.event.length);
+
+      notify2.off(notify1);
+
+      console.log('@notify3', notify3.listeners.event.length);
+
+      notify2.off(notify3);
+
+      console.log('@notify3', notify3.listeners.event.length);
+
+      const func2 = () => { };
+
+      notify1 = new Notify({
+         on: {
+            event: [func1, func1, func2]
+         }
+      });
+
+      console.log('@notify1.handlers', notify1.handlers.event.length);
+
+      notify1.off({
+         event: func1
+      }, notify2);
+
+      console.log('@notify1.handlers', notify1.handlers.event.length);
+
+      notify1.off({
+         event: func1
+      });
+
+      console.log('@notify1.handlers', notify1.handlers.event.length);
+
+      notify1.on({
+         event: func1
+      });
+
+      console.log('@notify1.handlers', notify1.handlers.event.length);
+
+      notify1.off({
+         event: [func1, func2]
+      });
+
+      console.log('@notify1.handlers', notify1.handlers);
    });
 });
